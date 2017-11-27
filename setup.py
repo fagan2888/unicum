7a5deeacf6dac165c73342f4ea9b1d8205bf324a
pbrisk <pbrisk@users.noreply.github.com>
2017-11-27 02:21:56 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     url='https://github.com/pbrisk/unicum',
     bugtrack_url='https://github.com/pbrisk/unicum/issues',
     license='Apache License 2.0',
-    packages=['unicum', 'unicum.genericfactory'],
+    packages=['unicum'],
     long_description=codecs.open('README.rst', encoding='utf-8').read(),
     platforms='any',
     classifiers=[
